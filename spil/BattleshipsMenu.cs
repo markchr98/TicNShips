a8231135a86f64a028c26e3be6c529c5dd1cda37
markchr98 <mark4@live.dk>
2017-10-03 14:32:13 +0200

    The basics of menu

--- spil/BattleshipsMenu.cs
+++ spil/BattleshipsMenu.cs
@@ -8,9 +8,32 @@ namespace spil
 {
     class BattleshipsMenu
     {
+        
         internal void Show()
         {
-            throw new NotImplementedException();
+            string input = "";
+            bool Battleships=true;
+            while (Battleships)
+            {
+                Menu();
+                
+
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        break;
+                    case "0":
+                        break;
+                }
+            }
+        }
+
+        internal void Menu()
+        {
+            Console.WriteLine("Type 1 to start a new game");
+            Console.WriteLine("Type 0 to quit");
+
         }
     }
 }
