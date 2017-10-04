486903e7ee041c6715f7b7293eb5d07b9464b467
Simon Aagaard <simo136g@edu.eal.dk>
2017-10-04 11:22:39 +0200

    Tests tilhørende Battleships

---
+++ Test/BattleshipTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Battleships;
+
+namespace BattleshipTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
