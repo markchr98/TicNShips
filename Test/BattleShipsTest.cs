853bcd2423a1513242bf93174842838b295567ee
markchr98 <mark4@live.dk>
2017-10-05 11:32:55 +0200

    Test class creation

---
+++ Test/BattleShipsTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using spil;
+
+namespace Test
+{
+    [TestClass]
+    class BattleShipsTest
+    {
+        [TestMethod]
+
+    }
+}
